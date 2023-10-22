b7d026b09ad633e4db0cf79d6a2d7451acd9632d
Vinicius Mendes <vinicius.mendes@loadsmart.com>
2023-10-22 19:03:31 -0300

    Implement locust with docker

---
+++ locust/locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/ht/")
