cc79e07f82fe04b6adfcefd6b9f9dfade9d7dd40
Vinicius Mendes <vinicius.mendes@loadsmart.com>
2023-10-22 22:15:02 -0300

    Create receita model

--- receitas/receitas/models.py
+++ receitas/receitas/models.py
@@ -1,3 +1,8 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Receita(models.Model):
+    titulo = models.CharField(max_length=255)
+    ingredientes = ArrayField(base_field=models.CharField(max_length=255))
+    modo_preparo = models.TextField()
