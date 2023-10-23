102f46fda74707015d88abb670764011d5152f25
Vinicius Mendes <vinicius.mendes@loadsmart.com>
2023-10-23 15:18:56 -0300

    Black.

--- receitas/receitas/migrations/0001_initial.py
+++ receitas/receitas/migrations/0001_initial.py
@@ -5,20 +5,31 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Receita',
+            name="Receita",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('titulo', models.CharField(max_length=255)),
-                ('ingredientes', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=255), size=None)),
-                ('modo_preparo', models.TextField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("titulo", models.CharField(max_length=255)),
+                (
+                    "ingredientes",
+                    django.contrib.postgres.fields.ArrayField(
+                        base_field=models.CharField(max_length=255), size=None
+                    ),
+                ),
+                ("modo_preparo", models.TextField()),
             ],
         ),
     ]
